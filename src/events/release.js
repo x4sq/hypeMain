9d67ab4d7dbb48630c2aa9c8c31102f0dd5457d4
x4sq <ryan110507@icloud.com>
2022-02-12 23:23:00 -0600

    Change release.js to not delete messages

--- src/events/release.js
+++ src/events/release.js
@@ -5,12 +5,7 @@ client.on('messageCreate', (message) => {
     let messageContent = message.content
     const msgreply = 'DUALITY has no confirmed release date. Please see <#850479152275783701> for further information.'
 
-    if(!messageContent.includes('release')) return;
-    if(messageContent.includes('release')) return message.reply(msgreply).then(msg => {
-        const message1 = msg
-        setTimeout(() => msg.delete(), 5000)
-        setTimeout(() => message.delete(), 5000)
-        
-    })
+    if(!messageContent.includes('release')) return message.reply(msgreply);
+
 ;
 })
