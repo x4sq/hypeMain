3e5e5e181aac6d5307a3c61c35aa7849561c849d
x4sq <ryan110507@icloud.com>
2022-02-13 10:57:28 -0600

    fix

--- src/events/release.js
+++ src/events/release.js
@@ -4,8 +4,9 @@ client.on('messageCreate', (message) => {
     if(message.author.bot) return;
     let messageContent = message.content
     const msgreply = 'DUALITY has no confirmed release date. Please see <#850479152275783701> for further information.'
+    
 
-    if(!messageContent.includes('release')) return message.reply(msgreply);
+    if(messageContent.includes('release')) return message.reply(msgreply);
 
 ;
 })
